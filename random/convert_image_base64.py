7b3734e434df8bad7f70a0bfb14aeaea1ac95507
Chris Durrans <durranschristopher@gmail.com>
2021-03-17 10:05:09 -0600

    Create convert image to base64

---
+++ random/convert_image_base64.py
@@ -0,0 +1,27 @@
+"""
+example usage
+python convert_image_base64.py --image="" --save=""
+"""
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image', required=True, type=int, default=20,
+                    help='Image to convert')
+parser.add_argument('--save', required=True, type=int, default=500,
+                    help='Save Location')
+args = parser.parse_args()
+
+# unpack
+image_location = args.image
+save_location = args.save)
+
+import base64
+
+with open(image_location, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+
+f = open(save_location,"wb")
+f.write(encoded_string)
+f.close()
